1a0efe28fd3d141191f1c096289932a5e0ecd6b9
MOJEED KUSIMO <mkusimo90@gmail.com>
2021-01-21 04:15:15 +0100

    Connected to heroku and created db on heroku

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 
 const client = new Client({
-    connectionString: 'postgresql://mojeedkusimo:root@localhost/api-exercise-users'
+    connectionString: process.env.DATABSE_URL || 'postgresql://mojeedkusimo:root@localhost/api-exercise-users'
 })
 
 client.connect();
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const db = require("./db");
 
 const router = express.Router();
 
